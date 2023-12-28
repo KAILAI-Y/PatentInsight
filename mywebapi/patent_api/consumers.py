eae5da55de62ad80d79bf8ed67add012d1b57b6f
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-12-28 18:35:51 -0500

    stream output

--- mywebapi/patent_api/consumers.py
+++ mywebapi/patent_api/consumers.py
@@ -18,19 +18,24 @@ class GPTConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         search_keyword = data.get("searchKeyword")
         base64_images = data.get("base64Images", [])
-        # print(base64_images)
         prompt_text = f"以下是关于搜索词“{search_keyword}”在专利集合中的搜索结果，以下图片可能涉及这个专利的1.年度分布，2.领域创新主体计量/分布，3.领域合作网络及测度中任何一种图。作为一名数据分析师，请根据获得的图的类型描述图中的具体信息并进行分析， 比如峰谷值。字数控制在50字左右。"
 
         # 异步调用同步函数
         response = await self.call_openai_api(prompt_text, base64_images)
 
-        # 提取并返回响应数据
-        generated_text = response.choices[0].message.content
-        last_period_index = generated_text.rfind("。")
-        if last_period_index != -1:
-            generated_text = generated_text[: last_period_index + 1]
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                await self.send_async(
+                    text_data=json.dumps({"message": chunk.choices[0].delta.content})
+                )
 
-        await self.send(text_data=json.dumps({"message": generated_text}))
+        # 提取并返回响应数据
+        # generated_text = response.choices[0].message.content
+        # last_period_index = generated_text.rfind("。")
+        # if last_period_index != -1:
+        #     generated_text = generated_text[: last_period_index + 1]
+
+        # await self.send(text_data=json.dumps({"message": generated_text}))
 
     @database_sync_to_async
     def call_openai_api(self, prompt_text, base64_images):
@@ -48,6 +53,10 @@ class GPTConsumer(AsyncWebsocketConsumer):
             model="gpt-4-vision-preview",
             messages=[{"role": "user", "content": messages}],
             max_tokens=300,
+            stream=True,
         )
 
         return response
+
+    async def send_async(self, text_data):
+        await self.send(text_data=text_data)
--- mywebapi/patent_api/static/JS/gpt-websocket.js
+++ mywebapi/patent_api/static/JS/gpt-websocket.js
@@ -2,30 +2,35 @@ var socket
 
 document.addEventListener('DOMContentLoaded', function () {
   socket = new WebSocket('ws://' + window.location.host + '/ws/gpt/')
+
   socket.onopen = function (e) {
     console.log('WebSocket 连接成功')
   }
+
   socket.onmessage = function (e) {
     var data = JSON.parse(e.data)
+    // console.log('Received WebSocket message: ', data)
     var message = data.message
-    document.getElementById('write-area').innerHTML = ''
-    typeWriter(message, 'write-area')
+    document.getElementById('write-area').innerHTML += message
+    // document.getElementById('write-area').innerHTML = ''
+    // typeWriter(message, 'write-area')
   }
+
   socket.onclose = function (e) {
     console.error('WebSocket 连接意外关闭')
   }
 })
 
-function typeWriter(text, elementId, speed = 50) {
-  console.log('typeWriter called with text: ' + text)
-  let i = 0
-  function typing() {
-    if (i < text.length) {
-      console.log('Adding character: ' + text.charAt(i))
-      document.getElementById(elementId).innerHTML += text.charAt(i)
-      i++
-      setTimeout(typing, speed)
-    }
-  }
-  typing()
-}
+// function typeWriter(text, elementId, speed = 50) {
+//   console.log('typeWriter called with text: ' + text)
+//   let i = 0
+//   function typing() {
+//     if (i < text.length) {
+//       console.log('Adding character: ' + text.charAt(i))
+//       document.getElementById(elementId).innerHTML += text.charAt(i)
+//       i++
+//       setTimeout(typing, speed)
+//     }
+//   }
+//   typing()
+// }
