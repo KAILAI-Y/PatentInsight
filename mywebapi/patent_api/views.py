cb9821f9b890124bc3f9f9239b12623d001cd144
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2024-01-08 21:52:07 -0500

    update feature words from models

--- mywebapi/patent_api/views.py
+++ mywebapi/patent_api/views.py
@@ -1,3 +1,5 @@
+from heapq import nlargest
+import heapq
 import os
 from django.core.paginator import Paginator
 from django.http import HttpResponse, JsonResponse
@@ -16,7 +18,6 @@ from io import BytesIO
 import base64
 from base64 import b64decode
 from wordcloud import WordCloud
-import jieba
 import jieba.analyse
 import jieba.posseg as pseg
 from itertools import combinations
@@ -253,34 +254,37 @@ def gpt_request(request):
         return JsonResponse({"error": "Invalid JSON"}, status=400)
 
 
-def extract_keywords_from_patent(patent, topK=5):
-    text = patent.title + " " + patent.abstract
+# extract keywords using jieba
+# def extract_keywords_from_patent(patent, topK=5):
+#     text = patent.title + " " + patent.abstract
 
-    stopwords_path = os.path.join(
-        settings.BASE_DIR, "patent_api", "static", "baidu_stopwords.txt"
-    )
+#     stopwords_path = os.path.join(
+#         settings.BASE_DIR, "patent_api", "static", "baidu_stopwords.txt"
+#     )
 
-    # 设置结巴分词的停用词
-    jieba.analyse.set_stop_words(stopwords_path)
+#     # 设置结巴分词的停用词
+#     jieba.analyse.set_stop_words(stopwords_path)
 
-    # 进行分词和词性标注
-    words = pseg.cut(text)
+#     # 进行分词和词性标注
+#     words = pseg.cut(text)
 
-    # 提取名词
-    nouns = [
-        word
-        for word, flag in words
-        if flag.startswith("n") and word not in stopwords_path
-    ]
+#     # 提取名词
+#     nouns = [
+#         word
+#         for word, flag in words
+#         if flag.startswith("n") and word not in stopwords_path
+#     ]
 
-    combined_nouns = " ".join(nouns)
+#     combined_nouns = " ".join(nouns)
 
-    keywords = jieba.analyse.textrank(combined_nouns, topK=topK, withWeight=False)
-    # print(keywords)
+#     keywords = jieba.analyse.textrank(combined_nouns, topK=topK, withWeight=False)
+#     # print(keywords)
 
-    return keywords
+#     return keywords
 
 
+##################################
+# fetch featurewords from models
 def generate_wordcloud_from_keywords(keywords_list, font_path):
     # 将关键词列表合并为一个字符串
     combined_keywords = " ".join(keywords_list)
@@ -305,23 +309,42 @@ def generate_wordcloud_from_keywords(keywords_list, font_path):
     return image_base64
 
 
+def get_top_keywords(patents, topK=100):
+    # 创建一个字典来存储所有关键词和它们的总权重
+    keyword_weights = {}
+
+    # 遍历所有专利，累加权重
+    for patent in patents:
+        featurewords = patent.featurewords.split(";")
+        probs = [float(p) for p in patent.probs.split(";")]
+
+        for word, weight in zip(featurewords, probs):
+            if word not in keyword_weights:
+                keyword_weights[word] = weight
+            else:
+                keyword_weights[word] += weight
+
+    # 获取权重最高的topK个关键词
+    top_keywords = nlargest(topK, keyword_weights, key=keyword_weights.get)
+    # 返回这些关键词及其权重
+    return {word: keyword_weights[word] for word in top_keywords}
+
+
 def generate_wordcloud_view(request):
     query = request.GET.get("q", "")
     patents = search(query)
 
-    font_path = os.path.join(
-        settings.BASE_DIR, "patent_api", "static", "font", "SimSun.ttf"
-    )
-
     if patents.exists():
-        all_keywords = []
-        for patent in patents:
-            keywords = extract_keywords_from_patent(patent)
-            all_keywords.extend(keywords)
+        top_keywords = get_top_keywords(patents, 100)
 
-        # 获取 Base64 编码的词云图像
+        # 获取字体路径
+        font_path = os.path.join(
+            settings.BASE_DIR, "patent_api", "static", "font", "SimSun.ttf"
+        )
+
+        # 生成词云图像的 Base64 编码
         wordcloud_image_base64 = generate_wordcloud_from_keywords(
-            all_keywords, font_path
+            top_keywords, font_path
         )
     else:
         wordcloud_image_base64 = None
@@ -331,20 +354,26 @@ def generate_wordcloud_view(request):
     )
 
 
-def calculate_cooccurrence(all_keywords):
+def calculate_cooccurrence(featurewords_list):
     cooccurrence_counts = defaultdict(int)
-    for keywords in all_keywords:
-        # 计算每篇专利中关键词的共现关系
-        for keyword1, keyword2 in combinations(set(keywords), 2):
-            cooccurrence_counts[(keyword1, keyword2)] += 1
-            cooccurrence_counts[(keyword2, keyword1)] += 1
+    for featurewords in featurewords_list:
+        words = featurewords.split(";")
+        # Count co-occurrences
+        for word1, word2 in combinations(set(words), 2):
+            cooccurrence_counts[(word1, word2)] += 1
+            cooccurrence_counts[(word2, word1)] += 1
     return cooccurrence_counts
 
 
 def create_network_graph(cooccurrence_counts):
+    # 创建一个空的无向图
     G = nx.Graph()
-    for (word1, word2), count in cooccurrence_counts.items():
-        G.add_edge(word1, word2, weight=count)
+
+    # 遍历共现关系字典，添加边到图中
+    for (word1, word2), weight in cooccurrence_counts.items():
+        # 添加边，其中word1和word2是节点，weight是共现次数或权重
+        G.add_edge(word1, word2, weight=weight)
+
     return G
 
 
@@ -366,12 +395,46 @@ def create_community_node_colors(graph, communities):
     node_colors = []
     for node in graph:
         current_community_index = 0
-        for community in communities:
+        # for community in communities:
+        for i, community in enumerate(communities):
             if node in community:
-                node_colors.append(colors[current_community_index])
+                # node_colors.append(colors[current_community_index])
+                node_colors.append(colors[i % len(colors)])
                 break
             current_community_index += 1
     return node_colors
+
+
+# def create_community_node_colors(graph, communities):
+#     # 定义一个足够大的颜色列表或循环使用的颜色
+#     colors = [
+#         "#D4FCB1",
+#         "#CDC5FC",
+#         "#FFC2C4",
+#         "#F2D140",
+#         "#BCC6C8",
+#         "#1f78b4",
+#         "#b2df8a",
+#         "#33a02c",
+#         "#fb9a99",
+#         "#e31a1c",
+#         "#fdbf6f",
+#         "#ff7f00",
+#         "#cab2d6",
+#         "#6a3d9a",
+#         "#ffff99",
+#         "#b15928",
+#     ]
+
+#     # 创建一个字典来映射每个节点到它的社区编号
+#     community_map = {}
+#     for idx, community in enumerate(communities):
+#         for node in community:
+#             community_map[node] = idx % len(colors)  # 使用取余数来循环颜色
+
+#     # 为每个节点分配颜色
+#     node_colors = [colors[community_map[node]] for node in graph]
+#     return node_colors
 
 
 # function to plot graph with node colouring based on communities
@@ -379,7 +442,7 @@ def visualize_communities(graph, communities):
     node_colors = create_community_node_colors(graph, communities)
     modularity = round(nx.community.modularity(graph, communities), 6)
     title = f"Community Visualization of {len(communities)} communities with modularity of {modularity}"
-    pos = nx.spring_layout(graph, k=0.5, iterations=100)
+    pos = nx.spring_layout(graph, k=0.8, iterations=100)
 
     plt.title(title)
     nx.draw(
@@ -394,18 +457,53 @@ def visualize_communities(graph, communities):
     )
 
 
-def keyword_network(query, patents):
+def limit_cooccurrences(cooccurrence, max_connections=5):
+    # 创建一个新的字典来存储限制后的共现关系
+    limited_cooccurrence = defaultdict(dict)
+
+    # 为每个词收集共现对
+    for (word1, word2), weight in cooccurrence.items():
+        limited_cooccurrence[word1][word2] = weight
+
+    # 限制每个词的共现对数量
+    for word, connections in limited_cooccurrence.items():
+        # 如果共现对的数量超过最大限制，则只保留权重最高的几个
+        if len(connections) > max_connections:
+            top_connections = heapq.nlargest(
+                max_connections, connections.items(), key=lambda x: x[1]
+            )
+            limited_cooccurrence[word] = dict(top_connections)
+
+    # 转换回原始格式
+    final_cooccurrence = {}
+    for word1, connections in limited_cooccurrence.items():
+        for word2, weight in connections.items():
+            final_cooccurrence[(word1, word2)] = weight
+
+    return final_cooccurrence
+
+
+def keyword_network(patents):
     if not patents:
         return JsonResponse({"error": "No patents found"})
     else:
-        all_keywords = [extract_keywords_from_patent(patent) for patent in patents]
-        cooccurrence = calculate_cooccurrence(all_keywords)
+        # all_keywords = [extract_keywords_from_patent(patent) for patent in patents]
+        all_featurewords = [patent.featurewords for patent in patents]
 
-        G = create_network_graph(cooccurrence)
+        cooccurrence = calculate_cooccurrence(all_featurewords)
+        sorted_cooccurrences = sorted(
+            cooccurrence.items(), key=lambda item: item[1], reverse=True
+        )
+        # top_cooccurrences = dict(sorted_cooccurrences[:2000])
+        top_cooccurrences = limit_cooccurrences(
+            dict(sorted_cooccurrences[:2000]), max_connections=5
+        )
+
+        print(len(top_cooccurrences))
+        G = create_network_graph(top_cooccurrences)
+
         communities = detect_communities(G)
-        # print("Detected communities:", communities)
 
-        # 为可视化设置画布和图像保存路径
         plt.figure(figsize=(30, 15))
 
         # 绘制网络图并保存
@@ -413,7 +511,7 @@ def keyword_network(query, patents):
         img_data = BytesIO()
         plt.savefig(img_data, format="png")
         plt.close()
-        img_data.seek(0)  # 移动到数据的开始位置
+        img_data.seek(0)
         img_base64 = base64.b64encode(img_data.read()).decode("utf-8")
 
         return img_base64
@@ -423,6 +521,6 @@ def word_network_view(request):
     query = request.GET.get("q", "")
     patents = search(query)
 
-    img_base64 = keyword_network(query, patents)
-    # print(img_base64)
+    img_base64 = keyword_network(patents)
+
     return render(request, "word_network.html", {"img_base64": img_base64})
